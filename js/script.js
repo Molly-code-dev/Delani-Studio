6b409c6319689b9770188655cd4381548ee1e89c
Molly-code-dev <mollymumbi42@gmail.com>
2020-07-30 11:18:53 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -99,8 +99,3 @@ $(document).ready(function () {
     });
 
 });
-$("p").hover(function () {
-    $(this).css("background-color", "yellow");
-}, function () {
-    $(this).css("background-color", "pink");
-});
